c3601cda02cb0bf0cb8de88be8c3186a4560f050
Kishor J K <kishorkonaraddi123@gmail.com>
2023-12-19 21:19:46 +0530

    Linked List: Inserting a node at the beginning

---
+++ L_L_1.c
@@ -0,0 +1,46 @@
+// Linked List: Inserting a node at the Beginning
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head; // global variable
+
+void Insert(int x)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = head;
+    head = temp;
+}
+
+void print()
+{
+    struct Node *temp = head;
+    printf("the list is:");
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    head = NULL; // empty list
+    printf("how meny numbars? ");
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the numbar: ");
+        scanf("%d", &x);
+        Insert(x);
+        print();
+    }
+}
